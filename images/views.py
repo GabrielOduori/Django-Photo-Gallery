a7042f2a16ed84963c1ce477ddb1c29494051999
Gabriel Oduori <gabriel.oduori@gmail.com>
2019-05-08 16:57:31 +0300

    App views
    Created the first view function which is currently responsible for rendering the home page

--- images/views.py
+++ images/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, Http404
+import datetime as dt
+from .models import Image
 
 # Create your views here.
+def index(request):
+    return render(request,'index.html')
