3ee7871b39fe1088a05ea00157407120f9bcef57
Gabriel Oduori <gabriel.oduori@gmail.com>
2019-05-08 16:13:27 +0300

    Models
    Created models for the three classes for the application namely, location, category and image itself. Methods for each mmodel class are also created and will be completed in the course of the project

--- images/models.py
+++ images/models.py
@@ -1,3 +1,68 @@
 from django.db import models
 
 # Create your models here.
+
+    
+    
+class Location(models.Model):
+    name = models.CharField(max_length =80)
+    def __str__(self):
+        return self.name
+    
+    def save_location():
+        pass
+    
+    def delete_location():
+        pass
+    
+
+class Category(models.Model):
+    name = models.CharField(max_length =80)
+    
+    def __str__(self):
+        return self.name
+    
+    
+    def save_category():
+            pass
+    
+    def delete_category():
+        pass
+    
+    
+
+class Image(models.Model):
+    name = models.CharField(max_length =80)
+    description = models.TextField()
+    location = models.ForeignKey(Location)
+    category = models.ForeignKey(Category)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    article_image = models.ImageField(upload_to = 'photos/')
+    
+    def save_image():
+        pass
+    
+    
+    def delete_image():
+        pass
+    
+    
+    
+    def update_image():
+        pass
+    
+    
+    def get_image_by_id(id):
+        pass
+    
+    def search_image(category):
+        pass
+    
+    def filter_by_location(location):
+        pass
+    
+    
+    
+    
+    
+    
