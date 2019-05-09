2f32dbb4d25901dd7f88c40a266bda8655ebd0c5
Gabriel Oduori <gabriel.oduori@gmail.com>
2019-05-09 22:27:03 +0300

    Admin configurations
    Registered application models to the admin so that they can be availabe ain the admin interface.

--- images/admin.py
+++ images/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Image, Location, Category
 
 # Register your models here.
+
+
+
+
+
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ['name','category','location','pub_date']
+    ordering = ['pub_date']
+    
+    
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    ordering = ['name']
+    
+    
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    ordering = ['name']
+    
+    
+admin.site.register(Image, ImageAdmin)
+admin.site.register(Location, LocationAdmin)
+admin.site.register(Category, CategoryAdmin)
