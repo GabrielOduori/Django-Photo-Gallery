536cea64f7b949c913966754018e9678f8d8b6cb
Gabriel Oduori <gabriel.oduori@gmail.com>
2019-05-09 22:45:04 +0300

    View funtions
    Added additional view funtions for the gallery, abouts us and a single page view

--- images/views.py
+++ images/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse, Http404
 import datetime as dt
 from .models import Image
@@ -6,3 +6,26 @@ from .models import Image
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+
+
+def gallery(requst):
+    
+    gallery = Image.objects
+    return render(requst, 'gallery/gallery.html', {'gallery':gallery})
+
+
+
+def about_us(request):
+    return render(request, 'about/about.html')
+
+
+
+def blog(request):
+    pass
+
+
+def single_image_view(request,id):
+    pass
+
+
