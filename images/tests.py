6730b23ae97635a79f066ead9789e71acd366bd7
Gabriel Oduori <gabriel.oduori@gmail.com>
2019-05-09 22:48:07 +0300

    Tests
    Added test classes for the three models classes in the database.

--- images/tests.py
+++ images/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import Image, Location, Category
+import datetime as dt
 
 # Create your tests here.
+
+class ImageTestClass(TestCase):
+    pass
+
+
+class LocationTestClass(TestCase):
+    pass
+
+
+class CategoryTestClass(TestCase):
+    pass
