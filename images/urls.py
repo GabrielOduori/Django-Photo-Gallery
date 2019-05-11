768a448f67b91cb031de0acf6d793044159f2d18
Gabriel Oduori <gabriel.oduori@gmail.com>
2019-05-11 22:12:03 +0300

    Added a link to the search box to point to the required end page

--- images/urls.py
+++ images/urls.py
@@ -5,7 +5,15 @@ from django.conf.urls.static import static
 
 urlpatterns =[
     # url(r'^$', views.welcome, name = 'welcome'),
-    url(r'^$', views.index, name = 'indexPage'),
+    url(r'^$', views.index, name = 'home'),
+    url(r'^gallery/$', views.gallery, name = 'gallery'),
+    url(r'^gallery/<int:image_id>/$', views.single_image_details, name='image_details'),
+    url(r'^about/$', views.about_us, name = 'about'),
+    url(r'^search/$', views.search_images, name = 'search_images'),
+    url(r'^category/sports/$', views.sports, name = 'sports'),
+    url(r'^category/technology/$', views.technology, name = 'technology'),
+    url(r'^category/nature/$', views.nature, name = 'nature'),
+    url(r'^category/entertainment/$', views.entertainment, name = 'entertainment'),
 
 ]
 if settings.DEBUG:
