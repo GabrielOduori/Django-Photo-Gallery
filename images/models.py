6c015a0360ad1edf765406e952eb04eff17b038c
Gabriel Oduori <gabriel.oduori@gmail.com>
2019-05-11 22:00:44 +0300

    Models
    Added a class method to search images by title in the models.
    Added functions to save and delete images in teh model.

--- images/models.py
+++ images/models.py
@@ -2,56 +2,78 @@ from django.db import models
 
 # Create your models here.
 
-    
-    
 class Location(models.Model):
-    name = models.CharField(max_length =80)
+    name = models.CharField(max_length =80, unique='True')
+    
+    class Meta:
+        ordering = ["name"]
+        verbose_name = "Location"
+        verbose_name_plural = "Locations"
+        
+        
     def __str__(self):
         return self.name
     
-    def save_location():
-        pass
+    def save_location(self):
+        self.save()
+        
     
-    def delete_location():
-        pass
+    def delete_location(self):
+        self.delete()
     
 
 class Category(models.Model):
-    name = models.CharField(max_length =80)
+    name = models.CharField(max_length =80, unique='True')
+    
+    class Meta:
+        ordering = ["name"]
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
     
     def __str__(self):
         return self.name
     
     
-    def save_category():
-            pass
+    def save_category(self):
+            self.save()
     
-    def delete_category():
-        pass
+    def delete_category(self):
+        self.delete()
     
     
 
 class Image(models.Model):
     name = models.CharField(max_length =80)
     description = models.TextField()
-    location = models.ForeignKey(Location)
-    category = models.ForeignKey(Category)
+    location = models.ForeignKey(Location, related_name='location', on_delete=models.DO_NOTHING)
+    category = models.ForeignKey(Category, related_name='category', on_delete=models.DO_NOTHING)
     pub_date = models.DateTimeField(auto_now_add=True)
-    article_image = models.ImageField(upload_to = 'photos/')
+    photo = models.ImageField(upload_to = 'photos/')
     
-    def save_image():
+    class Meta:
+        ordering = ["pub_date"]
+        verbose_name = "Image"
+        verbose_name_plural = "Images"
+    
+    def summary(self):
+        return self.description[:100]+" ......"
+    
+    
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
+    
+    def save_image(self):
+        self.save()
+    
+    
+    def delete_image(self):
+        self.delete()
+    
+    def update_image(self):
         pass
+        
     
-    
-    def delete_image():
-        pass
-    
-    
-    
-    def update_image():
-        pass
-    
-    
+
     def get_image_by_id(id):
         pass
     
@@ -59,6 +81,20 @@ class Image(models.Model):
         pass
     
     def filter_by_location(location):
+        pass
+    
+    
+    @classmethod
+    def search_by_title(cls,search_term):
+        searched_images = cls.objects.filter(title__icontains = search_term)
+        return searched_images
+    
+    @classmethod
+    def filter_by_location(location):
+        pass
+    
+    
+    def image_details(request,id):
         pass
     
     
