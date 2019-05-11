727a0bb90ee605a40532f3d3c6859f4b3bc157c4
Gabriel Oduori <gabriel.oduori@gmail.com>
2019-05-11 22:19:41 +0300

    Added test classes for image class as wel as two methods of saving adn deleting in the class.

--- images/tests.py
+++ images/tests.py
@@ -4,13 +4,32 @@ import datetime as dt
 
 # Create your tests here.
 
-class ImageTestClass(TestCase):
-    pass
-
-
 class LocationTestClass(TestCase):
-    pass
+    # Set up method
+    def setUp(self):
+        self.loc1= Location(name = 'Nairobi')
+        
+# Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.loc1,Location))
 
 
 class CategoryTestClass(TestCase):
-    pass
+    def setUp(self):
+        self.cat1 = Category(name = 'Sport')
+
+
+
+
+class ImageTestClass(TestCase):
+    def setUp(self):
+        # Creating a new editor and saving it
+        self.image1= Location(name = 'Nairobi')
+        self.image1.save_location()
+        
+        self.new_image= Image(name = 'Amboseli',description = 'This is a random post',location = self.image1)
+        self.new_image.save()
+
+
+
+
