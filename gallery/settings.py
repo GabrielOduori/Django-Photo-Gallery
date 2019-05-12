d8378154c07d42e2055ac0976d727f56ff8490d5
Gabriel Oduori <gabriel.oduori@gmail.com>
2019-05-12 12:55:01 +0300

    Updated settings.py config settings.

--- gallery/settings.py
+++ gallery/settings.py
@@ -106,7 +106,7 @@ if config('MODE')=="dev":
            'USER': config('DB_USER'),
            'PASSWORD': config('DB_PASSWORD'),
            'HOST': config('DB_HOST'),
-           'PORT': '5432',
+           'PORT': '',
        }
        
    }
