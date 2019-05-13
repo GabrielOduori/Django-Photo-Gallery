6716d888d7b3725045be72cef424992673d69ca8
Gabriel Oduori <gabriel.oduori@gmail.com>
2019-05-13 08:38:27 +0300

    Updated settings configurations

--- gallery/settings.py
+++ gallery/settings.py
@@ -28,7 +28,7 @@ MODE = config("MODE",default = "dev")
 SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool)
 # DEBUG = True
 
 # ALLOWED_HOSTS = []
