848988bb42f18c6daaa95b5c1b0b377efd164e52
Gabriel Oduori <gabriel.oduori@gmail.com>
2019-05-14 10:38:34 +0300

    Updated image search function

--- images/models.py
+++ images/models.py
@@ -85,7 +85,7 @@ class Image(models.Model):
     
     @classmethod
     def search_by_category(cls,search_term):
-        searched_images = cls.objects.filter(category__icontains = search_term)
+        searched_images = cls.objects.filter(category__name__icontains = search_term)
         return searched_images
     
     @classmethod
