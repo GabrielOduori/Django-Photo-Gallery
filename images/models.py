c0952fabb98ca10759dd143a504f793bdb372e2a
Gabriel Oduori <gabriel.oduori@gmail.com>
2019-05-14 09:43:41 +0300

    Updated the search by category function which was not initially working.

--- images/models.py
+++ images/models.py
@@ -84,8 +84,8 @@ class Image(models.Model):
     
     
     @classmethod
-    def search_by_title(cls,search_term):
-        searched_images = cls.objects.filter(title__icontains = search_term)
+    def search_by_category(cls,search_term):
+        searched_images = cls.objects.filter(category__icontains = search_term)
         return searched_images
     
     @classmethod
